58cf50ca8c2f2894ed7b925debc1f1b30a9695f0
seasaint <seasaint@naver.com>
2016-03-13 17:25:32 +0900

    create project

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  gitTestProject
+//
+//  Created by jong minlee on 2016. 3. 13..
+//  Copyright © 2016년 jgcompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print("ddd")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
